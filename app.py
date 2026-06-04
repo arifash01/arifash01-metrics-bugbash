423012e96f770bd131be4ccc765ee799b32e3dff
Kevin Halk <khalk@google.com>
2026-06-04 12:42:38 -0400

    Add basic HTTP server implementation in app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+
+PORT = 8000  # You can change the port number here
+
+# this is another change for another CL
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"serving {PORT}")
+    httpd.serve_forever()
